fa18b57de9500c075d6c15f9f1949baef46a3dd3
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-08-31 17:06:55 -0400

    d

--- backoffice/server.ts
+++ backoffice/server.ts
@@ -1,13 +1,20 @@
-async function handleConn(conn) {
+import { serveDir } from "https://deno.land/std@0.200.0/http/file_server.ts";
+
+async function handleConn(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
 
-  for await (const e of httpConn) {
-    e.respondWith(await handle(e.request));
-  }
+  for await (const e of httpConn) { e.respondWith(await handle(e.request)); }
 }
 
 async function handle(req) {
   if (req.headers.get("upgrade") != "websocket") {
+    const pathname = new URL(req.url).pathname;
+    
+    if (pathname.startsWith("/asset")) return serveDir(req, { fsRoot: new URL('.', import.meta.url).pathname + "/asset" });
+    // Build a readable stream so the file doesn't have to be fully loaded into
+    // memory while we send it
+    const readableStream = file.readable;
+
     return new Response(await Deno.readFile(new URL('.', import.meta.url).pathname + '/index.html'));
   }
 
@@ -25,5 +32,3 @@ async function handle(req) {
 }
 
 const listener = Deno.listen({ hostname: "localhost", port: 8080 });
-
-for await (const conn of listener) { handleConn(conn); }
