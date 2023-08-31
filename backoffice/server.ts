fad25598c12aa8f733a091dfa76c6846b9d443f7
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-08-31 12:43:39 -0400

    Update server.ts

--- backoffice/server.ts
+++ backoffice/server.ts
@@ -1,41 +1,43 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
 import { dishesData } from './data.ts'; // Import your data
 
-const server = serve({ port: 80 });
+const server = serve({ port: 80 }), Routes = new Map();
+Routes.set('/ws', function(req){
+  const { acceptWebSocket, acceptable } = ws(Deno.upgradeWebSocket(req));
+  console.log(acceptWebSocket, acceptable)
+  if (acceptable(req)) {
+    acceptWebSocket({
+      conn: req.conn,
+      bufReader: req.r,
+      bufWriter: req.w,
+      headers: req.headers,
+    })
+    .then(handleWebSocket);
+  }
+})
+Routes.set('/', function(req){
+  const body = await Deno.readFile('static/index.html');
+  req.respond({ body });
+})
+Routes.set('/styles.css', getRessource)
+Routes.set('/client.js', getRessource)
 
 for await (const req of server) {
   // Handle HTTP requests
   switch (true) {
-  case req.url === '/ws':
-    const { acceptWebSocket, acceptable } = Deno.upgradeWebSocket(req);
-    console.log(acceptWebSocket, acceptable)
-    if (acceptable(req)) {
-      acceptWebSocket({
-        conn: req.conn,
-        bufReader: req.r,
-        bufWriter: req.w,
-        headers: req.headers,
-      })
-        .then(handleWebSocket);
-      }
-    break;
   
-  case req.url === '/':
-    const body = await Deno.readFile('static/index.html');
-    req.respond({ body });
-    break;
-  
-  case req.url === '/styles.css':
-  case req.url === '/client.js':
-    const filePath = `static${req.url}`;
-    const headers = new Headers();
-    headers.set('Content-Type', 'text/css'); // Adjust Content-Type if needed
-    const body = await Deno.readFile(filePath);
-    req.respond({ body, headers });
-    break;
+  case req.url === :
+  case req.url === :
+    
   }
 }
-
+function getRessource(req){
+  const filePath = `static${req.url}`;
+  const headers = new Headers();
+  headers.set('Content-Type', 'text/css'); // Adjust Content-Type if needed
+  const body = await Deno.readFile(filePath);
+  req.respond({ body, headers });
+}
 async function handleWebSocket(ws: WebSocket) {
   for await (const event of ws) {
   if (typeof event === 'string') {
