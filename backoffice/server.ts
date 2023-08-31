ac128465663c51eceb862280b2acb9851af767e4
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-08-31 19:43:13 -0400

    check socket args

--- backoffice/server.ts
+++ backoffice/server.ts
@@ -1,20 +1,16 @@
 console.log('start')
 import { serveDir } from "https://deno.land/std@0.200.0/http/file_server.ts";
 
-async function handleConn(conn: Deno.Conn) {
-  const httpConn = Deno.serveHttp(conn);
-  console.log('handleConn')
-  for await (const e of httpConn) { e.respondWith(await handle(e.request)); }
-}
-function getPath(...args){
-  return new URL(...args).pathname
-}
+async function handleConn(conn: Deno.Conn) { for await (const e of Deno.serveHttp(conn)) e.respondWith(await handle(e.request)); }
+
+function getPath(...args){ return new URL(...args).pathname }
+
 async function handle(req) {
 
-  console.log('handle')
   if (req.headers.get("upgrade") != "websocket") {
-    const pathname = getPath(req.url), dirName = getPath('.', import.meta.url);
-    if(pathname.startsWith("/assets")) return serveDir(req, { fsRoot: dirName });
+    const dirName = getPath('.', import.meta.url);
+
+    if(getPath(req.url).startsWith("/assets")) return serveDir(req, { fsRoot: dirName });
 
     return new Response(await Deno.readFile(dirName + '/index.html'));
   }
@@ -23,6 +19,7 @@ async function handle(req) {
   const { socket, response } = Deno.upgradeWebSocket(req);
   socket.onopen = () => console.log("socket opened");
   socket.onmessage = (e) => {
+    console('arg =>',e)
     console.log("socket message:", e.data);
     socket.send(new Date().toString());
   };
@@ -34,4 +31,3 @@ async function handle(req) {
 
 const listener = Deno.listen({ hostname: "localhost", port: 8080 });
 for await (const conn of listener) { handleConn(conn); }
-console.log('end')
