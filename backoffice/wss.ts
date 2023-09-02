323ac1784caf2aced085f73e823bef26a8ad63ec
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-09-02 12:18:37 -0400

    tidying

--- backoffice/wss.ts
+++ backoffice/wss.ts
@@ -1,33 +1,31 @@
-const
-	Items = new Map,
-	channel = new BroadcastChannel('available'),
-	sockets = new Set<WebSocket>()
-
+const Items = new Map, sockets = new Set<WebSocket>();
 
 function broadcast(data){
   let i = 0, size = sockets.size;
-  if(size < 2) console.log('less than 2 connection');
+	if(size < 2) console.log('less than 2 connection');
   else console.log(size, ' connection')
-  e.target != channel && channel.postMessage(res.body)
-  sockets.forEach(socket => (send(socket, data), i++))
+	sockets.forEach(socket => (send(socket, data), i++))
   console.log(size == i)
 }
 
 function send(socket, data) { socket.send(JSON.stringify(data)) }
 
+function updateSockets(action, socket) {
+  const track = []
+  track.push(sockets.size)
+  sockets[action](socket)
+  track.push(sockets.size)
+  return track
+}
 export function ws({ socket, response }) {
 	
 	socket.onopen = () => {
-	  const before = sockets.size
-	  sockets.add(socket)
-	  const after = sockets.size
-	  console.log("socket opened", before,after)
+	  console.log("socket opened", ...updateSockets('add', socket))
 	};
 
-	socket.onmessage = channel.onmessage = (e) => {
+	socket.onmessage = (e) => {
 	  let data, res;
-    console.log(e)
-
+  
 	  try { data = JSON.parse(e.data); } catch (error) { data = e.data }
   
 	  const dataType = typeof data;
@@ -45,8 +43,7 @@ export function ws({ socket, response }) {
   
 	socket.onerror = (e) => console.log("socket errored:", e.message);
 	socket.onclose = () => {
-		console.log("socket closed")
-		sockets.delete(socket)
+		console.log("socket closed", ...updateSockets('delete', socket))
 	};
 	return response;
 }
