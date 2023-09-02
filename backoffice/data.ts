ff3e0f412c55c26e62251c2d0d024e28f2881f07
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-09-02 17:22:29 -0400

    d

--- backoffice/data.ts
+++ backoffice/data.ts
@@ -1,14 +1,9 @@
 // Simulated data storage for dishes
 export const items = [
-  { id: 1, titre: 'Dish A', detail: "The marvelously delicious dish A", prix: 10.99, image: false, type: "dishes" },
-  { id: 2, titre: 'Dish B', detail: "The marvelously delicious dish B", prix: 8.99,image: false, type: "dishes" },
-  { id: 3, titre: 'Drink A', detail: "The marvelously delicious drink A", prix: 5.99, image: false, type: "drinks" },
-  { id: 4, titre: 'Drink B', detail: "The marvelously delicious drink B", prix: 4.99,image: false, type: "drinks" },
+  { id: crypto.randomUUID(), titre: 'Dish A', detail: "The marvelously delicious dish A", prix: 10.99, image: false, type: "dishes" },
+  { id: crypto.randomUUID(), titre: 'Dish B', detail: "The marvelously delicious dish B", prix: 8.99,image: false, type: "dishes" },
+  { id: crypto.randomUUID(), titre: 'Drink A', detail: "The marvelously delicious drink A", prix: 5.99, image: false, type: "drinks" },
+  { id: crypto.randomUUID(), titre: 'Drink B', detail: "The marvelously delicious drink B", prix: 4.99,image: false, type: "drinks" },
 ]
 
-export const available = [
-  { target: 1, stock: 16 },
-  { target: 2, stock: 8 },
-  { target: 3, stock: 4 },
-  { target: 4, stock: 2 },
-]
+export const available = [ 16, 8, 4, 2 ].map((stock, i) => ({ target: items[i].id, stock }))
