2961c4d9d164b39a368fa33df557c65a72557874
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-09-02 17:01:10 -0400

    add: create request

--- backoffice/data.ts
+++ backoffice/data.ts
@@ -2,8 +2,8 @@
 export const items = [
   { id: 1, titre: 'Dish A', detail: "The marvelously delicious dish A", prix: 10.99, image: false, type: "dishes" },
   { id: 2, titre: 'Dish B', detail: "The marvelously delicious dish B", prix: 8.99,image: false, type: "dishes" },
-  { id: 3, titre: 'Drink A', detail: "The marvelously delicious drink A", prix: 10.99/2, image: false, type: "drinks" },
-  { id: 4, titre: 'Drink B', detail: "The marvelously delicious drink B", prix: 8.99/2,image: false, type: "drinks" },
+  { id: 3, titre: 'Drink A', detail: "The marvelously delicious drink A", prix: 5.99, image: false, type: "drinks" },
+  { id: 4, titre: 'Drink B', detail: "The marvelously delicious drink B", prix: 4.99,image: false, type: "drinks" },
 ]
 
 export const available = [
